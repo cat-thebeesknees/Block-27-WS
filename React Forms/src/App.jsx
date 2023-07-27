391e16626358a27bd6ecf17c7f2ff6e87d78d178
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-07-26 23:18:18 -0400

    completed steps 1 through 5 on tab 2. First tab is complete.

--- React Forms/src/App.jsx
+++ React Forms/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import SignUpForm from './components/SignUpForm'
+import Authenticate from './components/Authenitcate'
 
-function App() {
+export default function App() {
   
 
   return (
     <>
+    <SignUpForm />
+    <Authenticate />
       
     </>
-  )
+  );
 }
 
-export default App
+
---
+++ React Forms/src/components/SignUpForm.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function SignUpForm() {
+
+    return <h2>Sign Up!</h2>;
+
+}
