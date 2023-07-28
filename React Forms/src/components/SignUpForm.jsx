2a495273972abfc7fd2eb64cd3c00aa58a698cff
Catherine Hilton <126924004+cat-thebeesknees@users.noreply.github.com>
2023-07-27 20:17:24 -0400

    fetch to API is in the function

--- React Forms/src/components/SignUpForm.jsx
+++ React Forms/src/components/SignUpForm.jsx
@@ -1,8 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    return <h2>Sign Up!</h2>;
+  async function handleSubmit(event) {
+    event.preventDefault();
 
+    try {
+        const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+        const result = await response.json();
+        console.log(result);
+      
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
